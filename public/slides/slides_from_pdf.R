3c39fa2345946c7c3432e6bba088e0e1bf98bea9
Jay Brophy <jay@iMac-2019.local>
2020-09-29 09:51:33 -0400

    new public

--- public/slides/slides_from_pdf.R
+++
@@ -1,4 +0,0 @@
-# convert pdf to slides
-library(pdftools)
-pdf_convert("Brophy_CTO1.pdf", format = "jpeg", pages = NULL,
-            filenames = NULL, dpi = 300, opw = "", upw = "", verbose = TRUE)
